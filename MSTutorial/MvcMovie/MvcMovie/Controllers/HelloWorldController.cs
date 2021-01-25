0163f9ceb054b3487fa3e9d76c4f28942af9526e
sbpark88 <saebyulpark88@gmail.com>
2021-01-25 14:04:13 +0900

    MS .NET MVC Tutorial study. I hvae learned 'Controller' and 'View'. I will study 'Model' later.

---
+++ MSTutorial/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        //public string Index()
+        //{
+        //    return "HelloWorld Index String return";
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name, int ID = 1) {
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 2)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+
+            return View();
+        }
+    }
+}
